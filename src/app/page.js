c2b8508acacdcf214cccdca7563691f8e16e81d4
hs9uoj <hs9uoj@gmail.com>
2024-12-23 18:38:17 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,5 @@
-// src/app/page.js
-import RelayControl from '@/components/RelayControl';
-
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-100 py-8">
-      <RelayControl />
-    </main>
-  );
+    <div>Test Page</div>
+  )
 }
